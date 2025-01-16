e0501147c1a5fcb62e2d10adf26c3bb79a051769
elvisuriel <urielmarciales@gmil.com>
2025-01-15 20:31:07 -0500

    ajustes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com"], // Permite cargar imágenes desde este dominio
   },
-  output: "standalone",
+  output: "export", // Generar contenido estático
 };
 
 export default nextConfig;
